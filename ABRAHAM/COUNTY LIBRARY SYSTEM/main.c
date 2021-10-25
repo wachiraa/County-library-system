68a8321d24dc5f23042ab11b79a4c1bcd079426c
wachiraa <91315384+wachiraa@users.noreply.github.com>
2021-10-25 20:43:06 +0300

    Add files via upload

---
+++ ABRAHAM/COUNTY LIBRARY SYSTEM/main.c
@@ -0,0 +1,28 @@
+/*
+  PROJECT : County Library management system
+  AUTHOR  : Abraham Karuru
+  DATE    :24,Oct 2021
+  COMPILER:GNU GCC
+  LANGUAGE:C99
+  LICENSE :MIT
+
+
+*/
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tCounty Library\n");
+    printf("How are you Mr Abraham?");
+    printf("Welcome Mr Abraham.");
+    printf ("What would you like to do?\n");
+    printf("1.Add new User\n");
+    printf("2.Add Book.\n");
+    printf("Selected action: ")
+    return 0;
+}
